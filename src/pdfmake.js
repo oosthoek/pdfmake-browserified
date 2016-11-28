ab149d59e97151b666e4fe24897043771a529caa
Megan Oosthoek <oosthoek@umich.edu>
2016-11-28 17:01:48 -0500

    added roboto italics bold tff

--- src/pdfmake.js
+++ src/pdfmake.js
@@ -13,7 +13,7 @@ var defaultClientFonts = {
 		normal: 'Roboto-Regular.ttf',
 		bold: 'Roboto-Medium.ttf',
 		italics: 'Roboto-Italic.ttf',
-		bolditalics: 'Roboto-Italic.ttf'
+		bolditalics: 'Roboto-BoldItalic.ttf'
 	}
 };
 
